fdd74bcfda267a39397430433870fc88fe1f6b3e
andresbravog <andresbravog@gmail.com>
2014-12-14 18:56:47 +0100

    Style the menu

--- src/js/controllers/main_controller.js
+++ src/js/controllers/main_controller.js
@@ -23,4 +23,20 @@ angular.module('Furgospot.controllers.Main', [])
   return {
     template: "hello you"
   };
-});
+})
+
+.directive("markMapCurrent",function(){
+  var mapOptions = {
+      center: new google.maps.LatLng(-34.397, 150.644),
+      zoom: 8,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+  var map = new google.maps.Map(document.getElementById("map_canvas"),
+                                mapOptions);
+  return {
+    template: "hello you"
+  };
+})
+
+
+;
