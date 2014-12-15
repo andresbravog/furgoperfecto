369f0642112e3c558ba87be81972657c0d809751
andresbravog <andresbravog@gmail.com>
2014-12-15 06:43:13 +0100

    more style and directives

--- src/js/controllers/main_controller.js
+++ src/js/controllers/main_controller.js
@@ -26,15 +26,9 @@ angular.module('Furgospot.controllers.Main', [])
 })
 
 .directive("markMapCurrent",function(){
-  var mapOptions = {
-      center: new google.maps.LatLng(-34.397, 150.644),
-      zoom: 8,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-  var map = new google.maps.Map(document.getElementById("map_canvas"),
-                                mapOptions);
+  
   return {
-    template: "hello you"
+    template: "Done"
   };
 })
 
